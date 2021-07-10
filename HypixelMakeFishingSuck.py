6e5a1f37b992758ca8fe24734da860467f2061fa
Memeprograms <79157805+Memeprograms@users.noreply.github.com>
2021-07-09 21:07:02 -0500

    Update HypixelMakeFishingSuck.py

--- HypixelMakeFishingSuck.py
+++ HypixelMakeFishingSuck.py
@@ -1,9 +1,40 @@
-lol = 1
+import random
+import math
+import time
+import pyautogui
+import keyboard
+
 while True:
-    lol = lol + 1
-    print("Hi", lol + 1)
-    if lol == 9999:
-        break
-    else:
-        continue
+
+    randomnumber = random.randint(1,2000)
+
+    if randomnumber == range(1,50):
+        pyautogui.click(button="right")
+        print("Right clicked")
+
+    if randomnumber == range(50,75):
+        pyautogui.click("s")
+        print("S clicked")
+
+    if randomnumber == range(75,100):
+        pyautogui.click("w")
+        print("W clicked")
+
+
+    if randomnumber == 169:
+        pyautogui.keyDown("alt")
+        pyautogui.keyDown("f4")
+        pyautogui.keyUp("alt")
+        pyautogui.keyUp("f4")
+        print("Game closed")
+
+
+    def cancel():
+        randomnumber = 0
     
+    keyboard.is_pressed("k", cancel())
+
+    if randomnumber == 0:
+        break
+
+    time.sleep(1)
